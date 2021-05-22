5a09fbf510a918701fba6ea618121417fa90cfa5
Kevin Decker <KevinDecker64@gmail.com>
2021-05-22 01:59:58 -0500

    insalls axios; adds proxy in package.json; creates temporary IndexPage querying data from Django successfully

--- frontend/src/pages/IndexPage/IndexPage.jsx
+++ frontend/src/pages/IndexPage/IndexPage.jsx
@@ -1,12 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Row, Col, Card } from "react-bootstrap";
 
+export default function IndexPage() {
+  const [photos, setPhotos] = useState([]);
 
-export default function IndexPage(){
-    console.log('photos')
-    return(
-        <>
-        <h1>Photo Index Page</h1>
+  useEffect(() => {
+    async function fetchPhotos() {
+      const { data } = await axios.get("/photos/");
+      setPhotos(data);
+    }
+    fetchPhotos();
+  }, []);
 
-        </>
-    )
+  console.log(photos);
+
+  return (
+    <div>
+      <h1>Photos Index Page</h1>
+      <Row>
+        {photos.map((photo) => (
+          <Col key={photo.id} sm={12} md={6} lg={4} xl={3}>
+            <Card className="my-3 p-3 rounded">
+              <Card.Body as="div">
+                <Card.Title as="div">
+                  <strong>{photo.user}</strong>
+                </Card.Title>
+                <Card.Text as="div">
+                  <div className="my-3">
+                    <strong>Img URL: </strong>
+                    {photo.url}
+                  </div>
+                </Card.Text>
+                <Card.Text as="div">
+                    <div className="my-3">
+                        {photo.location}
+                    </div>
+                </Card.Text>
+                <Card.Text as="div">
+                    <div className="my-3">
+                        {photo.caption}
+                    </div>
+                </Card.Text>
+                <Card.Text as="div">
+                    <div className="my-3">
+                        Likes: {photo.total_likes} | Comments: {photo.total_comments}
+                    </div>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
 }
