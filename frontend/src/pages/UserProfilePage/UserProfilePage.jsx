8359703bf4a3838f1f20991e774509b27edb39e5
andrewhuang192 <andrewhuang192@gmail.com>
2021-05-25 14:54:53 -0700

    quick updates

--- frontend/src/pages/UserProfilePage/UserProfilePage.jsx
+++ frontend/src/pages/UserProfilePage/UserProfilePage.jsx
@@ -2,11 +2,23 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import "./UserProfilePage.css";
-import { Row, Col, Card, CardGroup, CardColumns } from "react-bootstrap";
+import {
+  Row,
+  Col,
+  Card,
+  CardGroup,
+  CardColumns,
+  Modal,
+  Button,
+} from "react-bootstrap";
 
 export default function UserProfilePage({ user }) {
   const [allUsers, setAllUsers] = useState([]);
   const [photos, setPhotos] = useState([]);
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   //   useEffect(() => {
   //     async function fetchUser() {
@@ -32,107 +44,180 @@ export default function UserProfilePage({ user }) {
     fetchPhotos();
   }, []);
 
-  console.log(photos);
-  console.log(user);
-  console.log(allUsers);
+  const handleDeletePhoto = async (e, photoId) => {
+    e.preventDefault();
 
-  // users.map((user) => (
+    const options = {
+      url: "http://localhost:8000/photos/:id/delete",
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: {
+        photo: photoId
+      },
+    };
+    const response = await axios(options);
+    const token = response.data.token;
+    const user = response.data.user;
 
-  //   )
-  // )
+    localStorage.setItem("token", token);
+    
+    if (localStorage.getItem("token")) {
+      setPhotos(response);
+    }
+  };
 
   return (
     <>
-    <main className="profile-main pro-main pro-main-2">
-    <div className="profile-1 profile-3 prof-1">
-      {/* <h1> </h1> */}
-      <header className="header-1">
-        <CardGroup>
-          <Card>
-            <div className="profile-img-1">
-              <div className="profile-img-2">
-                <h1>
-                  
-                </h1>
-                <img className="pic-prof" src="{photo.url}" />
-                <span className="span-pic">
-                  
-                  </span>
-              </div>
-            </div>
-            <section className="profile-sec">
-                <div className="profile-div-1">
-            <Card.Body>
-              <Card.Title>
-                <h2 className="user-name name name1 name-profile">{user.username}</h2>
-                <br />
-                <div className="user-name name name1 name-profile">
-                  Last Login: {new Date(user.last_login).toLocaleDateString()}
+      <main className="profile-main pro-main pro-main-2">
+        <div className="profile-1 profile-3 prof-1">
+          {/* <h1> </h1> */}
+          <header className="header-1">
+            <CardGroup>
+              <Card>
+                <div className="profile-img-1">
+                  <div className="profile-img-2">
+                    <h1></h1>
+                    <img className="pic-prof" src="{photo.url}" />
+                    <span className="span-pic"></span>
+                  </div>
                 </div>
-                <br />
-              </Card.Title>
-              {/* <Card.Text>I love eating</Card.Text> */}
-              <div className="btn-1 btn-2 btn-3">
-              <Card.Link>
-                  <Link
-                    className="btn btn-xs"
-                    to={{
-                      pathname: "/profile/update",
-                      state: { user },
-                    }}
-                    user={user}
-                  >
-                    <button>Edit Profile</button>
-                  </Link>
-              </Card.Link>
-              </div>
-            </Card.Body>
-                </div>
-            </section>
-            
-            <Card.Footer>
-              <small className="text-muted">
-                Joined on {new Date(user.date_joined).toLocaleDateString()}
-              </small>
-            </Card.Footer>
-          </Card>
-        </CardGroup>
-      </header>
-      </div>
+                <section className="profile-sec">
+                  <div className="profile-div-1">
+                    <Card.Body>
+                      <Card.Title>
+                        <h2 className="user-name name name1 name-profile">
+                          {user.username}
+                        </h2>
+                        <br />
+                        <div className="user-name name name1 name-profile">
+                          Last Login:{" "}
+                          {new Date(user.last_login).toLocaleDateString()}
+                        </div>
+                        <br />
+                      </Card.Title>
+                      {/* <Card.Text>I love eating</Card.Text> */}
+                      <div className="btn-1 btn-2 btn-3">
+                        <Card.Link>
+                          <Link
+                            className="btn btn-xs"
+                            to={{
+                              pathname: "/profile/update",
+                              state: { user },
+                            }}
+                            user={user}
+                          >
+                            <button>Edit Profile</button>
+                          </Link>
+                        </Card.Link>
+                      </div>
+                    </Card.Body>
+                  </div>
+                </section>
 
-      <Row>
-        {photos.map((photo) => (
-          <CardColumns>
-            {photo.user === user.id ? (
-              <Card className="my-3 p-3 rounded">
-                <Card.Body as="div">
-                  <Card.Title as="div">
-                    <strong>{photo.user}</strong>
-                  </Card.Title>
-                  <Card.Text as="div">
-                    <img className="my-3" src={photo.url} />
-                  </Card.Text>
-                  <Card.Text as="div">
-                    <div className="my-3">{photo.location}</div>
-                  </Card.Text>
-                  <Card.Text as="div">
-                    <div className="my-3">{photo.caption}</div>
-                  </Card.Text>
-                  <Card.Text as="div">
-                    <div className="my-3">
-                      Likes: {photo.total_likes} | Comments:{" "}
-                      {photo.total_comments}
-                    </div>
-                  </Card.Text>
-                </Card.Body>
+                <Card.Footer>
+                  <small className="text-muted">
+                    Joined on {new Date(user.date_joined).toLocaleDateString()}
+                  </small>
+                </Card.Footer>
               </Card>
-            ) : (
-              console.log("bye")
+            </CardGroup>
+          </header>
+        </div>
+
+          {photos.map((photo) => (
+        <Row>
+              {photo.user === user.id ? (
+                <Card className="my-3 p-3 rounded">
+                  <Card.Body as="div">
+                    <Card.Title as="div">
+                      <Row>
+                        <Col>
+                          <strong>{photo.user}</strong>
+                        </Col>
+                        <Col></Col>
+                        <Col></Col>
+                        <Col>
+                          <CardGroup>
+                            <Button variant="light" onClick={handleShow}>
+                              <svg
+                                aria-label="More options"
+                                class="_8-yf5 "
+                                fill="#262626"
+                                height="16"
+                                viewBox="0 0 48 48"
+                                width="16"
+                              >
+                                <circle
+                                  clip-rule="evenodd"
+                                  cx="8"
+                                  cy="24"
+                                  fill-rule="evenodd"
+                                  r="4.5"
+                                ></circle>
+                                <circle
+                                  clip-rule="evenodd"
+                                  cx="24"
+                                  cy="24"
+                                  fill-rule="evenodd"
+                                  r="4.5"
+                                ></circle>
+                                <circle
+                                  clip-rule="evenodd"
+                                  cx="40"
+                                  cy="24"
+                                  fill-rule="evenodd"
+                                  r="4.5"
+                                ></circle>
+                              </svg>
+                            </Button>
+
+                            <Modal show={show} onHide={handleClose}>
+                              <Modal.Header>
+                                <Modal.Title>
+                                  Are you sure you want to delete this photo?
+                                </Modal.Title>
+                              </Modal.Header>
+                              <Modal.Footer>
+                                <Button
+                                  variant="secondary"
+                                  onClick={handleClose}
+                                >
+                                  Cancel
+                                </Button>
+                                <Button variant="primary" onClick={handleClose} onSubmit={handleDeletePhoto}>
+                                  Delete
+                                </Button>
+                              </Modal.Footer>
+                            </Modal>
+                          </CardGroup>
+                        </Col>
+                      </Row>
+                      <div className="my-3">{photo.location}</div>
+                    </Card.Title>
+
+                    <Card.Img as="div">
+                      <img className="my-3" src={photo.url} />
+                    </Card.Img>
+                    <Card.Text as="div"></Card.Text>
+                    <Card.Text as="div">
+                      <div className="my-3"><span><strong>{photo.user}</strong>   {photo.caption}</span></div>
+                    </Card.Text>
+                    <Card.Text as="div">
+                      <div className="my-3">
+                        Likes: {photo.total_likes} | Comments:{" "}
+                        {photo.total_comments}
+                      </div>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              ) : (
+                console.log("bye")
               )}
-          </CardColumns>
-        ))}
-      </Row>
-    </main>
-      </>
+        </Row>
+          ))}
+      </main>
+    </>
   );
 }
