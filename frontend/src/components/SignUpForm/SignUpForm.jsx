c1de806e0c0a32e136eb0ab938b8b46248c53ac9
Edith Rodriguez <edithrodriguez@Ediths-MacBook-Air.local>
2021-05-24 20:58:13 -0700

    sign up page and redirecting now working

--- frontend/src/components/SignUpForm/SignUpForm.jsx
+++ frontend/src/components/SignUpForm/SignUpForm.jsx
@@ -19,24 +19,37 @@ class SignupForm extends React.Component {
 
   render() {
     return (
-      <form onSubmit={e => this.props.handle_signup(e, this.state)}>
-        <h4>Sign Up</h4>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          name="username"
-          value={this.state.username}
-          onChange={this.handle_change}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          value={this.state.password}
-          onChange={this.handle_change}
-        />
-        <input type="submit" />
-      </form>
+      <article className="art">
+        <div className="gr27e">
+        <img src="https://i.imgur.com/bfjUjgq.png" width='280px' height='90px' />
+        <div className="signup-div">
+          <form className="sign-form" onSubmit={e => this.props.handle_signup(e, this.state)}>
+            <h2 className="sign-sub">Sign up to see photos and videos from your friends.</h2>
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handle_change}
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handle_change}
+            />
+            <input type="submit" />
+          </form>
+          </div>
+          <p className="sign-term">By signing up, you agree to our Terms , Data Policy and Cookies Policy .</p>
+        </div>
+        <div className="gr27e">
+        <div className="bot-div log-bot bottom-div bottom-log sign-bot">
+        <p className="bot-p">Already have an account? <a href="/login/">Login</a></p>
+        </div>
+      </div>  
+      </article>
     );
   }
 }
