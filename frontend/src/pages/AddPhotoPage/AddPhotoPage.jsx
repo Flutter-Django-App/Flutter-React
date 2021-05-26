5af458ec9d14e3e0dfaee66e7558eca01ee5fc83
andrewhuang192 <79735825+andrewhuang192@users.noreply.github.com>
2021-05-26 06:58:34 -0700

    Update AddPhotoPage.jsx

--- frontend/src/pages/AddPhotoPage/AddPhotoPage.jsx
+++ frontend/src/pages/AddPhotoPage/AddPhotoPage.jsx
@@ -4,6 +4,9 @@ import axios from "axios";
 import './AddPhotoPage.css'
 import { Form, Button } from "react-bootstrap";
 
+axios.defaults.xsrfCookieName='csrftoken'
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+
 export default function AddPhotoPage({user}) {
   const [showPhoto, setShowPhoto] = useState('Upload a Photo')
   const [formData, setFormData] = useState({
@@ -17,22 +20,41 @@ export default function AddPhotoPage({user}) {
     setFormData({
       ...formData,
       [e.target.name]: e.target.value,
+      
     });
+    console.log(formData)
   };
 
   const handleSubmit = async (e) => {
+    const photo_id = e.target.value
+    console.log(user.id)
+    console.log(photo_id)
+    console.log(formData.caption)
+    console.log(formData.location)
+    console.log(formData.url)
     e.preventDefault();
+    const options = {
+      url: `http://localhost:8000/photos/${user.id}/add_photo/`,
+      method: "POST",
+      headers: {
+        Authorization: `JWT ${localStorage.getItem("token")}`,
+      },
+      data: {
+        photo: photo_id,
+        user: user.id,
+        caption: formData.caption, 
+        location: formData.location,
+        url: formData.url,
+      },
+    };
     try {
-      console.log(formData.url)
-      // console.log(formData.caption)
-      // console.log(formData.location)
-      const res = await axios.post(`/photos/${user.id}/add_photo/`, formData);
-      console.log(res);
-      history.push("photos");
-    } catch (err) {
-      console.log(err);
+      const photo = await axios(options).then((response) => {
+        console.log('Response for submission=>', response);
+      });
+    } catch {
+      console.log('bleh')
     }
-  };
+  }
 
   return (
     <div className="add-photo">
